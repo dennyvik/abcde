e76341874fb14a20059cc6008874e03807f6b852
dennyvik <dennyvik@yahoo.com>
2020-05-17 20:58:46 +0700

    add header with logo and link

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPages from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 
 
 function App() {
   return ( 
     <div >
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} ></Route>
         <Route path='/shop' component={ShopPages} ></Route>

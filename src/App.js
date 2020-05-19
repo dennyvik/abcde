eee118807485bea04060aa79d88baa63f2859ccd
dennyvik <dennyvik@yahoo.com>
2020-05-19 23:08:21 +0700

    add Sign in Page form n style

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPages from './pages/shop/shop.component';
 import Header from './components/header/header.component';
+import SignInSignUp from './pages/sign-in-sign-up/sign-in-sign-up.component';
 
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} ></Route>
         <Route path='/shop' component={ShopPages} ></Route>
+        <Route path='/sign-in' component={SignInSignUp} ></Route>
       </Switch>
     </div>
   );
